2198da386b54a7066a3cac9a404cc2e37748c169
Kenneth Chien <kennethchien.kc@gmail.com>
2022-11-14 17:51:56 +0800

    Added list seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-# Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-# Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-# Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-# Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 uri = URI.open("https://tmdb.lewagon.com/movie/top_rated").read
 uri = JSON.parse(uri)
 uri["results"].each do |el|
@@ -19,3 +15,24 @@ uri["results"].each do |el|
     rating: el["vote_average"]
   )
 end
+
+file = URI.open("https://res.cloudinary.com/du2lmmkva/image/upload/v1668417291/development/comedy_llpu9p.jpg")
+list= List.create!(
+  name: "Comedy"
+)
+list.photo.attach(io: file, filename: "comedy_llpu9p.jpeg")
+file = URI.open("https://res.cloudinary.com/du2lmmkva/image/upload/v1668419258/development/sci-fi_n8j1ey.jpg")
+list= List.create!(
+  name: "Sci-Fi"
+)
+list.photo.attach(io: file, filename: "sci-fi_n8j1ey.jpeg")
+file = URI.open("https://res.cloudinary.com/du2lmmkva/image/upload/v1668419258/development/romance_brxdx9.jpg")
+list= List.create!(
+  name: "Romance"
+)
+list.photo.attach(io: file, filename: "romance_brxdx9.jpeg")
+file = URI.open("https://res.cloudinary.com/du2lmmkva/image/upload/v1668419258/development/horror_movie_liuddl.jpg")
+list= List.create!(
+  name: "Horror"
+)
+list.photo.attach(io: file, filename: "horror_movie_liuddl.jpeg")
